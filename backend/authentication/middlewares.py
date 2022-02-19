ce5621072df198154adccfceca8efee6e06fca76
Бакдаулет Болат <bakdauletbolat@Bakdaulets-MacBook-Air.local>
2022-02-20 03:46:42 +0600

    a

--- backend/authentication/middlewares.py
+++ backend/authentication/middlewares.py
@@ -1,6 +1,3 @@
-"""General web socket middlewares
-"""
-
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,12 +1,9 @@
 import os
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from authentication import routing,middlewares
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": middlewares.JwtAuthMiddlewareStack(
