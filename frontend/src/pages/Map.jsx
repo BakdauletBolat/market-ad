4abb2462f6f5868fe33b48b27c1d2ef6f50ca0e1
Бакдаулет Болат <bakdauletbolat@Bakdaulets-MacBook-Air.local>
2022-02-22 01:49:50 +0600

    a

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -39,7 +39,7 @@ function Map() {
 
     const chatSocket = new WebSocket(
         'ws://'
-        + '0.0.0.0:9001'
+        + '89.223.67.169:9001'
         + '/ws/online-users/'
         + `?token=${token}`
     );
